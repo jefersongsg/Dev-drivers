86f59808d837fd171dd12cc0d17c7cb78867b36f
jefersongsg <jefersonsilva5525@gmail.com>
2025-03-28 16:19:54 -0300

    adiçao de mais items e configurando os botoes e endicadpres

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 let prev = document.getElementById("buttonL");
 let next = document.getElementById("buttonR");
 let container = document.querySelector(".container");
-let cars = container.querySelectorAll(".list",  ".car");
+let car = container.querySelectorAll(".list .cars");
 let indicator = document.querySelector(".indicators");
 let dots = indicator.querySelectorAll("ul li");
 
 let active = 0;
-let firtPosition = 0;
-let lastPosition = cars.length - 1;
+let firstPosition = 0;
+let lastPosition = car.length - 1;
 
 next.onclick = () => {
-    let destiv = container.querySelector(".list" , ".car.active");
+    let destiv = container.querySelector(".list .cars.active");
     destiv.classList.remove("active")
     
     active = active + 1 > lastPosition ? 0 : active + 1;
-    console.log(active);
+    car[active].classList.add("active");
+    
+    let destiv2 = indicator.querySelector("ul li.active");
+    destiv2.classList.remove("active")
+    dots[active].classList.add("active");
 };
 
 prev.onclick = () => {
-    let destiv = container.querySelector(".list" , ".car.active");
+    let destiv = container.querySelector(".list .cars.active");
     destiv.classList.remove("active")
     
-    console.log(active);
-   
+    active = active - 1 < firstPosition ? lastPosition : active - 1;
+    car[active].classList.add("active");
+    
+    let destiv2 = indicator.querySelector("ul li.active");
+    destiv2.classList.remove("active")
+    dots[active].classList.add("active");
 };
